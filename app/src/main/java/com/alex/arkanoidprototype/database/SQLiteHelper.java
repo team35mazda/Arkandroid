8f4f7417491783fda23c2a36e8fc19e8f9bd68bd
techmic <michael_trem@hotmail.com>
2019-03-24 22:00:02 -0400

    DB Helper ajout des call pour la construction de la base de données

--- app/src/main/java/com/alex/arkanoidprototype/database/SQLiteHelper.java
+++ app/src/main/java/com/alex/arkanoidprototype/database/SQLiteHelper.java
@@ -5,8 +5,22 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import com.alex.arkanoidprototype.database.datamodel.NiveauItem;
+import com.alex.arkanoidprototype.database.datasource.CouleurDataSource;
+import com.alex.arkanoidprototype.database.datasource.DimensionBalleDataSource;
+import com.alex.arkanoidprototype.database.datasource.DimensionBalleItemDataSource;
+import com.alex.arkanoidprototype.database.datasource.DimensionBaseDataSource;
+import com.alex.arkanoidprototype.database.datasource.DimensionBaseItemDataSource;
+import com.alex.arkanoidprototype.database.datasource.ItemDataSource;
+import com.alex.arkanoidprototype.database.datasource.ItemMouvementDataSource;
+import com.alex.arkanoidprototype.database.datasource.ItemTypeDataSource;
+import com.alex.arkanoidprototype.database.datasource.MouvementDataSource;
+import com.alex.arkanoidprototype.database.datasource.NiveauDataSource;
+import com.alex.arkanoidprototype.database.datasource.NiveauItemCurrentMouvementDataSource;
+import com.alex.arkanoidprototype.database.datasource.NiveauItemCurrentPositionDataSource;
+import com.alex.arkanoidprototype.database.datasource.NiveauItemCurrentVisibilityDataSource;
 import com.alex.arkanoidprototype.database.datasource.NiveauItemDataSource;
+import com.alex.arkanoidprototype.database.datasource.PositionDataSource;
+import com.alex.arkanoidprototype.database.datasource.PositionItemDataSource;
 
 public class SQLiteHelper extends SQLiteOpenHelper {
 
@@ -19,16 +33,47 @@ public class SQLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase database) {
+        database.execSQL(CouleurDataSource.create());
+        database.execSQL(DimensionBalleDataSource.create());
+        database.execSQL(DimensionBalleItemDataSource.create());
+        database.execSQL(DimensionBaseDataSource.create());
+        database.execSQL(DimensionBaseItemDataSource.create());
+        database.execSQL(ItemDataSource.create());
+        database.execSQL(ItemMouvementDataSource.create());
+        database.execSQL(ItemTypeDataSource.create());
+        database.execSQL(MouvementDataSource.create());
+        database.execSQL(NiveauDataSource.create());
+        database.execSQL(NiveauItemCurrentMouvementDataSource.create());
+        database.execSQL(NiveauItemCurrentPositionDataSource.create());
+        database.execSQL(NiveauItemCurrentVisibilityDataSource.create());
         database.execSQL(NiveauItemDataSource.create());
+        database.execSQL(PositionDataSource.create());
+        database.execSQL(PositionItemDataSource.create());
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
         Log.w(SQLiteHelper.class.getName(),
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
-        db.execSQL(NiveauItemDataSource.drop());
-        onCreate(db);
+
+        database.execSQL(CouleurDataSource.drop());
+        database.execSQL(DimensionBalleDataSource.drop());
+        database.execSQL(DimensionBalleItemDataSource.drop());
+        database.execSQL(DimensionBaseDataSource.drop());
+        database.execSQL(DimensionBaseItemDataSource.drop());
+        database.execSQL(ItemDataSource.drop());
+        database.execSQL(ItemMouvementDataSource.drop());
+        database.execSQL(ItemTypeDataSource.drop());
+        database.execSQL(MouvementDataSource.drop());
+        database.execSQL(NiveauDataSource.drop());
+        database.execSQL(NiveauItemCurrentMouvementDataSource.drop());
+        database.execSQL(NiveauItemCurrentPositionDataSource.drop());
+        database.execSQL(NiveauItemCurrentVisibilityDataSource.drop());
+        database.execSQL(NiveauItemDataSource.drop());
+        database.execSQL(PositionDataSource.drop());
+        database.execSQL(PositionItemDataSource.drop());
+        onCreate(database);
     }
 }
 
